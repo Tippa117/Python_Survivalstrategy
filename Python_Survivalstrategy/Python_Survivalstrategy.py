f97e6066b6e1b116732cdf2c146efc3c05c8da4d
nitoiok630 <>
2022-03-22 17:41:15 +0900

    動くところまで

--- Python_Survivalstrategy/Python_Survivalstrategy.py
+++ Python_Survivalstrategy/Python_Survivalstrategy.py
@@ -3,15 +3,33 @@ import pygame as pg
 from pygame.locals import *
 import sys
 
+#class Creature:
+#    """
+#    """
+#    def __init__(self):
+#        self.born_x = 20
+#        self.born_y = 20
+#        self,born_size_x = 50
+#        self.born_size_y = 50
+    
+#    def create(self):
+#        pg.Rect(self.born_x, self.born_y, self.born_size_x, self.born_size_y)
+
 def main():
     pg.init() # 初期化
-    disp_w, disp_h = 800, 450 #画面サイズ横と縦
+    disp_w, disp_h = 800, 450 # 画面サイズ横と縦
     pg.display.set_mode((disp_w,disp_h),0,32)
     screen = pg.display.get_surface()
-    #screen = pygame.display.set_mode((disp_w,disp_h)) # ウィンドウサイズの指定
+    # screen = pygame.display.set_mode((disp_w,disp_h)) # ウィンドウサイズの指定
     pg.display.set_caption("Pygame Test") # ウィンドウの上の方に出てくる文字の指定
-    
-    #移動
+    # 画面更新速度
+    FPS = 30 
+    fpsClock = pg.time.Clock()
+
+    # 生き物オブジェクト生成
+    #creature1 = Creature()
+    creature1 = pg.Rect(20,20,50,50)
+
     x = 50
     dx = 10
 
@@ -19,14 +37,15 @@ def main():
         screen.fill((255,255,224,)) # 背景色の指定。RGB指定
 
         #描画処理
-        pg.draw.rect(screen, (255,0,0),(x,70,100,100))
-        if x >= 300 or x <= 0:
-            dx = -dx
-        x += dx
-        #time.sleep(0.05)
+        pg.draw.rect(screen, (255,0,0),creature1)
         pg.display.update() # 画面更新
+        fpsClock.tick(FPS)
 
-
+        if creature1.x >= (disp_w-50) or creature1.x <= 0:
+            dx = -dx
+        
+        creature1.x += dx
+        
         for event in pg.event.get(): # 終了処理
             if event.type == QUIT:
                 pg.quit()
